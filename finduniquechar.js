5b1790735bf04ef3609298332a558702379724f2
Thowhidul Mehraj seyam <sseyam450@gmail.com>
2026-06-16 01:16:42 +0600

    Add files via upload

---
+++ finduniquechar.js
@@ -0,0 +1,12 @@
+// set moethod 
+
+const s1 = "javascript";
+const s2 = [...new Set(s1)];
+console.log(s2);
+
+// spread operator 
+
+const s3 = "Expressjs";
+const s4 = Array.from(new Set(s3));
+
+console.log(s4);
